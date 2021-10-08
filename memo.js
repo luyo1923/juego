c592ddb2f35d6a2ec9a770ba3380ba05eed30a74
luyo <alluyoflores@itel.edu.ar>
2021-10-08 18:48:36 -0300

    mod de js cambiand el id y agregando ; a lo que falta y el html id

--- memo.js
+++ memo.js
@@ -1,7 +1,7 @@
 // variables
-let iconos = [] //donde se guardan los iconos
-let selecciones = [] //donde se guardan temporalmente las selecciones de las tarjetas
-generarTablero()
+let iconos = []; //donde se guardan los iconos
+let selecciones = []; //donde se guardan temporalmente las selecciones de las tarjetas
+generarTablero();
 function cargarIconos() {
     // generar cada uno de los elementos de los iconos
     // cadena de texto comillas simples
@@ -18,13 +18,13 @@ function cargarIconos() {
         '<i class="fas fa-chess-knight"></i>',
         '<i class="fas fa-chess"></i>',
         '<i class="fas fa-dice-d20"></i>',
-    ]
+    ];
 }
 function generarTablero() {
-    cargarIconos()
-    selecciones = []
-    let tablero = document.getElementById("tablero")
-    let tarjetas = []
+    cargarIconos();
+    selecciones = [];
+    let tablero = document.getElementById("tablero");
+    let tarjetas = [];
     
     for (let i = 0; i < 24; i++) {
         // texto representativo
@@ -40,46 +40,46 @@ function generarTablero() {
                 </div>
             </div>
         </div>        
-        `)
+        `);
         // si el contador "i" no es multiplo de 2 eliminar el primer elemento de iconos
         if (i % 2 == 1) {
-            iconos.splice(0, 1)
+            iconos.splice(0, 1);
         }
     }
     // desorganizar las cartas
-    tarjetas.sort(() => Math.random() - 0.5)
-    tablero.innerHTML = tarjetas.join(" ")
+    tarjetas.sort(() => Math.random() - 0.5);
+    tablero.innerHTML = tarjetas.join(" ");
     ocultar();
 }
 function ocultar(){
-    document.getElementById("nuevo-juego").style.display = "none";
+    document.getElementById("newJuego").style.display = "none";
 }
 // tarjetas seleccionadas y deseleccionadas
 function seleccionarTarjeta(i) {
-    let tarjeta = document.getElementById("tarjeta" + i)
+    let tarjeta = document.getElementById("tarjeta" + i);
     if (tarjeta.style.transform != "rotateY(180deg)") {
-        tarjeta.style.transform = "rotateY(180deg)"
-        selecciones.push(i)
+        tarjeta.style.transform = "rotateY(180deg)";
+        selecciones.push(i);
     }
     if (selecciones.length == 2) {
-        deseleccionar(selecciones)
-        selecciones = []
+        deseleccionar(selecciones);
+        selecciones = [];
     }
 }
 function deseleccionar(selecciones) {
     setTimeout(() => {
-        let trasera1 = document.getElementById("trasera" + selecciones[0])
-        let trasera2 = document.getElementById("trasera" + selecciones[1])
+        let trasera1 = document.getElementById("trasera" + selecciones[0]);
+        let trasera2 = document.getElementById("trasera" + selecciones[1]);
         // si no coinciden giran
         if (trasera1.innerHTML != trasera2.innerHTML) {
-            let tarjeta1 = document.getElementById("tarjeta" + selecciones[0])
-            let tarjeta2 = document.getElementById("tarjeta" + selecciones[1])
-            tarjeta1.style.transform = "rotateY(0deg)"
-            tarjeta2.style.transform = "rotateY(0deg)"
+            let tarjeta1 = document.getElementById("tarjeta" + selecciones[0]);
+            let tarjeta2 = document.getElementById("tarjeta" + selecciones[1]);
+            tarjeta1.style.transform = "rotateY(0deg)";
+            tarjeta2.style.transform = "rotateY(0deg)";
         }else{
             // si coinciden se cambia de color plum = "color ciruela"
-            trasera1.style.background = "plum"
-            trasera2.style.background = "plum"
+            trasera1.style.background = "plum";
+            trasera2.style.background = "plum";
         }
     }, 1000);
 }
